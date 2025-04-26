4a4fb420c26d9bf542e08223bfd4aa272892d1ee
watasuke102 <watasuke102@gmail.com>
2025-04-26 21:09:24 +0900

    update: Timer>adjust font size regarding the screen size

--- src/features/timer/Timer.tsx
+++ src/features/timer/Timer.tsx
@@ -5,7 +5,7 @@ const style = stylex.create({
   timerCounter: {
     display: 'block',
     textAlign: 'center',
-    fontSize: '25dvw',
+    fontSize: 'min(25dvw, 30dvh)',
     fontWeight: 'bold',
     fontFamily: 'Consolas, "Courier New", monospace',
   },
