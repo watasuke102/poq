c67a6cb6adf7f8418138ff880700206af42c41cc
watasuke102 <watasuke102@gmail.com>
2025-04-29 16:33:17 +0900

    fix: vite.config>add basedir

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,9 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react-swc";
-import stylex from "vite-plugin-stylex";
+import {defineConfig} from 'vite';
+import react from '@vitejs/plugin-react-swc';
+import stylex from 'vite-plugin-stylex';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/poq',
   plugins: [react(), stylex()],
 });
